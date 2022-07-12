02ef3dde28862ab61ed9b80317211bfefd6c31d3
Testing0124 <93730370+Testing0124@users.noreply.github.com>
2022-07-12 10:44:32 -0300

    Create f125d914ab539e1584.js

---
+++ f125d914ab539e1584.js
@@ -0,0 +1,8 @@
+const elem = require('./elements').elements;
+
+class f125d914ab539e1584 {
+
+    f125d914ab539e1584() {
+        cy.get(elem.f125d914ab539e1584).click()
+        cy.get('#switch-lg-social').should('be.checked')
+    }
